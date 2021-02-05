2eeab5fc63e73c84f837f0c85ee24a5f9e3c1f68
ludo046 <ludomoissinac@icloud.com>
2021-02-05 17:35:15 +0100

    produit.js

--- frontend/produit.js
+++ frontend/produit.js
@@ -1,47 +1,89 @@
-//recuperatio de l'id du produit 
-let param = new URLSearchParams(document.location.search);
-let id = param.get('id')
-console.log(id);
+function getOneTeddie() {    //fonction pour afficher le produit selectionner 
+    //recuperation de l'id du produit 
+    const param = new URLSearchParams(document.location.search);
+    const id = param.get('id');
+    console.log(id);
 
-function getOneTeddie(){    //fonction pour afficher le produit selectionner 
-    fetch(`http://localhost:3000/api/teddies/${id}`)
-    .then((response) => response.json())
-    .then(oneTeddie  => {
-        console.log(oneTeddie.colors);
-        //affichage des element du produit 
-        const name = document.querySelector('.teddie-name')
-        name.innerText = oneTeddie.name;
-        const img = document.querySelector('.img')
-        img.src = oneTeddie.imageUrl;
-        const description = document.querySelector('.description')
-        description.innerText = oneTeddie.description
-        const price = document.querySelector('.price')
-        price.innerText = 'prix :' + oneTeddie.price/100+`.00€`
-        //choix couleur 
-        const select = document.getElementById('color')
-        //boucle pour recuperer les options de chaque produit
-        for (let i in oneTeddie.colors){
-            select.innerHTML += `<option value='${oneTeddie.colors[i]}' selected='selected'>${oneTeddie.colors[i]}</option>`
+
+    return fetch(`http://localhost:3000/api/teddies/${id}`)
+        .then((response) => response.json())
+        .then(oneTeddie => oneTeddie);
+}
+
+
+// la fonction s'auto appelle au chargement de la page
+(async function createElements() { // la fonction sert uniquement a creer les elements du dom dynamiquement
+
+    const oneTeddie = await getOneTeddie(); // attend que la fonction lui retourne le resultat du fetch requetant l'api back-end
+
+    console.log(oneTeddie.colors);
+    //affichage des element du produit 
+    const name = document.querySelector('.teddie-name');
+    name.innerText = oneTeddie.name;
+    const img = document.querySelector('.img');
+    img.src = oneTeddie.imageUrl;
+    const description = document.querySelector('.description');
+    description.innerText = oneTeddie.description;
+    const price = document.querySelector('.price');
+    price.innerText = 'prix :' + oneTeddie.price / 100 + `.00€`;
+    //choix couleur 
+    const select = document.getElementById('color');
+    //boucle pour recuperer les options de chaque produit
+    for (let i in oneTeddie.colors) {
+        if (i < 1) {
+            select.innerHTML += `<option value='${oneTeddie.colors[i]}' selected='selected'>${oneTeddie.colors[i]}</option>`;
+        } else {
+            select.innerHTML += `<option value='${oneTeddie.colors[i]}'>${oneTeddie.colors[i]}</option>`;
+
         }
-        //recuperation de l'option 
-       let optionValue  = document.getElementById("color").value;
-       console.log(optionValue);
-        //recuperation de la quantité 
-       let quantity = document.querySelector('.input').value;
-       console.log(quantity);
-    })
+    }
+})();
+
+function getValues() {
+    //recuperation de l'option 
+    let optionValue = document.getElementById("color").value;
+    console.log(optionValue);
+    //recuperation de la quantité 
+    let quantity = document.querySelector('.input').value;
+    console.log(quantity);
+
+    let customerChoice = { // on créé un objet avec les valuers récupérées
+        optionValue: optionValue,
+        quantity: quantity
+    };
+
+    console.log('choix client', customerChoice); // on affiche dans la console pour verifier si l'objet n'est pas vide
+    return customerChoice; // on retourne l'objet
 }
-getOneTeddie() //appel de la fonction 
 
 
+const addCard = document.querySelector('.btn');
 
-function addToCard(){
-    const addcard = document.querySelector('.btn')
-        addcard.addEventListener('click', function(){
-            localStorage.setItem(name, oneTeddie.name);
-            localStorage.setItem(select,optionValue);
-            localStorage.setItem(quantity, quantity);
-            localStorage.setItem(price, oneTeddie.price);
-       })
-}
-addToCard()
+addCard.addEventListener('click', async function () {
+    const customerChoice = await getValues(); // on attend de recevoir les values (l'objet customerChoice)
+    const oneTeddie = await getOneTeddie();
+
+    //on cree un objet avec les information du produit 
+    let completeTeddies = {
+        name : oneTeddie.name,
+        select : customerChoice.optionValue,
+        quantity : customerChoice.quantity,
+        price : oneTeddie.price,
+    }
+    console.log(completeTeddies);
+
+    let card = JSON.parse(localStorage.getItem('teddie'));
+
+    if(!card){    //ajoute au tableau ci pas de panier 
+        let card = []
+        card.push(completeTeddies)
+        localStorage.setItem('teddie',JSON.stringify(card))
+    }else if (!card.some(teddie => teddie == teddie.id === completeTeddies.id)){    //si panier verifier que je n'ai pas le meme produit a l'interieur avant d'ajouter le nouveau 
+        card.push(completeTeddies)
+        localStorage.setItem('teddie',JSON.stringify(card))
+    }else{      //ci ej l'ai deja je l'enleve pour le remplacer 
+        const newCard = card.filter(teddie => teddie == teddie.id !== completeTeddies.id)
+        newCard.push(completeTeddies)
+        localStorage.setItem('teddie', JSON.stringify(newCard))
+    }
+});
